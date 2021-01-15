aefa6b5f91eb5c71b0667325f1c0eee9ef476dd9
Marvoro53 <marvinlara321@gmail.com>
2021-01-14 18:39:21 -0600

    added 4th and 5th task

--- about.js
+++ about.js
@@ -1,2 +1,8 @@
 document.body.style.fontFamily = 'Arial, sans-serif';
-     
+document.getElementById('nickname').textContent = 'Princess Bubblegum';
+document.getElementById('favorites').textContent = 'science, music, my candy subjects';
+document.getElementById('hometown').textContent = 'Candy Kingdom';
+var items = document.getElementsByTagName('li');
+for (var i = 0; i < items.length; i++) {
+    items[i].className = 'list-item';
+}
