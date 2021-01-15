29a0da7f9d380d6316385a77349451b9e4eaeb14
Marvoro53 <marvinlara321@gmail.com>
2021-01-14 18:49:05 -0600

    added info to js

--- about.js
+++ about.js
@@ -1,7 +1,7 @@
 document.body.style.fontFamily = 'Arial, sans-serif';
-document.getElementById('nickname').textContent = 'Princess Bubblegum';
-document.getElementById('favorites').textContent = 'science, music, my candy subjects';
-document.getElementById('hometown').textContent = 'Candy Kingdom';
+document.getElementById('nickname').textContent = 'Guero';
+document.getElementById('favorites').textContent = 'Music, Apples and Movies ';
+document.getElementById('hometown').textContent = 'Denver Co';
 var items = document.getElementsByTagName('li');
 for (var i = 0; i < items.length; i++) {
     items[i].className = 'list-item';
